1d42f7d9892fde676f0e2d9265e984c259eb476e
InsuranceApp <madhurya.malladi@gmail.com>
2016-06-07 17:29:37 +0530

    Update app.js
    Added Nano to operate on views for manufacturer API and model api

--- app.js
+++ app.js
@@ -15,15 +15,21 @@
 var express = require('express');
 var async = require('async');
 var Cloudant = require('cloudant');
+
 var cloudant = Cloudant({url: 'https://c8471542-8b25-4c18-8533-68457a121a4e-bluemix:98b2d738277220eede0580c59102e6574d39cabe6d08ba57e5f6d3a5d8555aab@c8471542-8b25-4c18-8533-68457a121a4e-bluemix.cloudant.com'});
+
+var nano = require('nano')('https://c8471542-8b25-4c18-8533-68457a121a4e-bluemix:98b2d738277220eede0580c59102e6574d39cabe6d08ba57e5f6d3a5d8555aab@c8471542-8b25-4c18-8533-68457a121a4e-bluemix.cloudant.com');
 
  //Database operations
 				var	  dbname = 'quotestore';
 				var manufacturerdbname = 'manufacturers';
+				var modelsdbname = 'models';
 					var  db = null;
 					var manufacturerdb = null;
+					var modelsdb = nano.db.use(modelsdbname);
 					var  db = cloudant.db.use(dbname);
-					var manufacturerdb = cloudant.db.use(manufacturerdbname);
+					var manufacturerdb = nano.db.use(manufacturerdbname);
+					
 
 var bodyParser = require('body-parser');
 
@@ -93,50 +99,48 @@ app.listen(appEnv.port, '0.0.0.0', function() {
 //Retrieve All Manufacturers
 /***************************************************************************************/ 
 
- app.get('/api/getallmanufacturers', urlencodedParser, function (req, res) {
+	 app.get('/api/getallmanufacturers', urlencodedParser, function (req, res) {
 		
-					var manufacturerList = function() {
-						
-					  	
-					  	 manufacturerdb.list(function(err, body) {
-					var result = [];
-						body.rows.forEach(function(doc) {
-      					console.log("Response from DB " , doc);
-      				
-      					var manufacturerid= doc.id;
-						manufacturerdb.get(manufacturerid, function(err, data) {
-							console.log("Individual Data:", data);
-							
-							result.push({id: data._id, manufacturer_name: data.manufacturer_name});
-							console.log("Result", result);
-
-						});
-
-					 });
-				
-					 });
-				
-				
-					  	
-					  };
-					
-					  console.log("Retrieving all manufacturers ");
-					  
-					  var resultFromDB = manufacturerList();
-					  
-					  console.log("resultFromDB", resultFromDB);
-					  
-						res.send(JSON.stringify(resultFromDB));
-												 
+		manufacturerdb.view('retrieveAllManufacturers', 'retrieveAllManufacturers', function(err, body)
+		{
+			if (!err) {
+		    var result = [];
+		      body.rows.forEach(function(doc) {
+		        result.push({id: doc.key, manufacturer_name: doc.value});
+		        
+		      });
+		      res.send(JSON.stringify(result));
+    }
 		});
+		
+	});
 	
-
-
-												 
 	
-				
-					
+/***************************************************************************************/
+//Retrieve All Models
+/***************************************************************************************/ 
 
-					
-				
-				
+	 app.get('/api/getModel/:manufacturer_name', urlencodedParser, function (req, res) {
+		
+	
+	console.log("Parameters from request", req.params.manufacturer_name);
+	
+	var manufacturer_name = req.params.manufacturer_name;
+
+		modelsdb.view('retrieveModel', 'retrieveModel', {key : manufacturer_name} , function(err, body)
+		{
+			if (!err) {
+		    var result = [];
+		    
+		      body.rows.forEach(function(doc) {
+		      	console.log("Doc from DB ", doc);
+		        result.push({id: doc.id, manufacturer_name: doc.key, model_name: doc.value.model});
+		        
+		      });
+		      res.send(JSON.stringify(result));
+    }
+		});
+		
+	});
+	
+	
